28bf8195274bc1fef5606917120d9a02be66234c
alexafiliadodigital <108899496+alexafiliadodigital@users.noreply.github.com>
2023-07-30 17:17:46 -0300

    commit terceiro site

---
+++ arquivo.js
@@ -0,0 +1,20 @@
+const menu=document.querySelector(".menu");
+const NavMenu=document.querySelector(".nav-menu");
+
+
+
+
+
+menu.addEventListener("click",(ativo)=>{
+
+menu.classList.toggle("ativo");
+NavMenu.classList.toggle("ativo");
+
+
+})
+
+
+
+
+
+
